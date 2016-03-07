5db00935e0c0ad9af24da809396814695beb6c56
Piia Hartikka <Piia@Piia-MBP-2.Elisa>
2016-03-07 15:51:48 +0200

    jjh

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,25 +3,25 @@ class RatingsController < ApplicationController
 
       #Muutokset nopeuttivat sivun lataamista n. 100-kertaisesti. 
 
-  		#@recent_ratings = Rating.recent
-      Rails.cache.write("recent ratings", Rating.recent) if fragment_exist?("recent ratings")
-      @recent_ratings = Rails.cache.read "recent ratings"
+  		@recent_ratings = Rating.recent
+      #Rails.cache.write("recent ratings", Rating.recent) if fragment_exist?("recent ratings")
+      #@recent_ratings = Rails.cache.read "recent ratings"
 
-      #@top_breweries = Brewery.top 3
-      Rails.cache.write("brewery top 3", Brewery.top(3)) if fragment_exist?("brewery top 3")
-      @top_breweries = Rails.cache.read "brewery top 3"
+      @top_breweries = Brewery.top 3
+      #Rails.cache.write("brewery top 3", Brewery.top(3)) if fragment_exist?("brewery top 3")
+      #@top_breweries = Rails.cache.read "brewery top 3"
 
-      #@top_beers = Beer.top 3
-      Rails.cache.write("beer top 3", Beer.top(3)) if fragment_exist?("beer top 3")
-      @top_beers = Rails.cache.read "beer top 3"
+      @top_beers = Beer.top 3
+      #Rails.cache.write("beer top 3", Beer.top(3)) if fragment_exist?("beer top 3")
+      #@top_beers = Rails.cache.read "beer top 3"
 
-      #@top_styles = Style.top 3
-      Rails.cache.write("style top 3", Style.top(3)) if fragment_exist?("style top 3")
-      @top_styles = Rails.cache.read "style top 3"
+      @top_styles = Style.top 3
+      #Rails.cache.write("style top 3", Style.top(3)) if fragment_exist?("style top 3")
+      #@top_styles = Rails.cache.read "style top 3"
 
-      #@top_users = User.top 3
-      Rails.cache.write("user top 3", User.top(3)) if fragment_exist?("user top 3")
-      @top_users = Rails.cache.read "user top 3"
+      @top_users = User.top 3
+      #Rails.cache.write("user top 3", User.top(3)) if fragment_exist?("user top 3")
+      #@top_users = Rails.cache.read "user top 3"
       
   	end
 
